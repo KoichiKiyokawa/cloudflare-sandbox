4fcaf420b84a67a3dabde65e301add3b3c70616a
Koichi Kiyokawa <koichi20110068@gmail.com>
2024-04-09 00:57:12 +0900

    chore: use @go-task/cli

--- remix-kysely-d1/scripts/generate-migration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-migration_name=$1
-if [ -z "$1" ]; then
-  read -p "Enter migration name: " migration_name
-fi
-
-pnpm atlas migrate diff --to file://db/schema.sql --dev-url "sqlite://file?mode=memory" $migration_name
--- remix-kysely-d1/scripts/generate-type-from-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf tmp.db
-sqlite3 tmp.db < db/schema.sql
-DATABASE_URL=tmp.db pnpm kysely-codegen
-rm -rf tmp.db
